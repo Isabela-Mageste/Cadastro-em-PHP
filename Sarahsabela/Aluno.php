bad43df6f590852943514bed21b48e36b5f3a990
Sarah Rabelo Araujo <117447148+LoiraDoTchan@users.noreply.github.com>
2024-10-21 11:40:07 -0300

    formulario inicial

---
+++ Sarahsabela/Aluno.php
@@ -0,0 +1,9 @@
+ <?php
+abstract class Aluno{ 
+    public $matricula;
+
+    public function confirmarrecebimento(){
+        echo  "<br/>Confirmado o recebimento";
+    } 
+}   
+?>
---
+++ Sarahsabela/Pessoa.php
@@ -0,0 +1,8 @@
+<?php
+class Pessoa{ 
+    $nome = $_POST['fname'];
+    echo $name;
+
+    public $cpf;
+}   
+?>
